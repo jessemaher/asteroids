df121f2de8c26e579e07865b3e17cc21f04da2e2
Jesse Maher <jessemaher15@gmail.com>
2026-06-04 16:18:35 -0600

    Added classes for sprites and rendering of drawing for player

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
+from player import * 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         # poll for events
@@ -13,11 +18,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        #This renders all the previous drawings, have it last if you want to see all the things you are "drawing"
         pygame.display.flip()
 
-    print("Starting Asteroids with pygame version: VERSION")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
