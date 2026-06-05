0e963edf978cc782954b06cdf81175ef16958692
Jesse Maher <jessemaher15@gmail.com>
2026-06-05 10:59:24 -0600

    Added bullets and cleaned up some organization on main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state, log_event
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -16,9 +17,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     #Create Player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 import pygame
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__ (self, x: float, y: float) -> None:
@@ -14,6 +15,15 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot_vector = pygame.Vector2(0, 1)
+        shot_rotated_vector = shot_vector.rotate(self.rotation)
+        shot_rotated_with_speed_vector = shot_rotated_vector * PLAYER_SHOOT_SPEED
+        shot.velocity += shot_rotated_with_speed_vector
+        #Streamlined Method would be
+            #shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
@@ -38,4 +48,10 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+    
+
+        
+
     
